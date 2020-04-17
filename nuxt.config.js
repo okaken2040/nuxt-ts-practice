7c2f6c7882c963f6833ecee4b950936ad15a1c9b
okaken2040 <tanpopo5518@gmail.com>
2020-04-17 21:01:20 +0900

    保存時に自動でESlintが走る設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -83,6 +83,15 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend(config, ctx) {
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
   }
 }
